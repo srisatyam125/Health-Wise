08e932e44c594f1c53110ba686b978c1af95c520
Aditya-Sharma-dev <itsadityasharma02@gmail.com>
2023-07-16 21:22:59 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,132 +1,119 @@
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config()
+  require("dotenv").config();
 }
 
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const initializePassport = require("./passport-config");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
 
-// Importing Libraies that we installed using npm
-const express = require("express")
-const app = express()
-const bcrypt = require("bcrypt") // Importing bcrypt package
-const passport = require("passport")
-const initializePassport = require("./passport-config")
-const flash = require("express-flash")
-const session = require("express-session")
-const methodOverride = require("method-override")
-
-app.use(express.static('views'));
+app.use(express.static("views"));
 
 initializePassport(
-    passport,
-    name => users.find(user => user.name === name),
-    id => users.find(user => user.id === id)
-    )
+  passport,
+  (name) => users.find((user) => user.name === name),
+  (id) => users.find((user) => user.id === id)
+);
 
+const users = [];
 
-
-const users = []
-
-app.use(express.urlencoded({extended: false}))
-app.use(flash())
-app.use(session({
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
-    resave: false, // We wont resave the session variable if nothing is changed
-    saveUninitialized: false
-}))
-app.use(passport.initialize()) 
-app.use(passport.session())
-app.use(methodOverride("_method"))
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
 
-// Configuring the register post functionality
-app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login",
-    failureFlash: true
-}))
+    failureFlash: true,
+  })
+);
 
-// Configuring the register post functionality
 app.post("/register", checkNotAuthenticated, async (req, res) => {
-
-    var s=req.body.name
-    var pass=req.body.password
-        // {alert("ONly letters");}
-        if(containsOnlyLetters(s) && passCheck(pass)){
-        try {
-            const hashedPassword = await bcrypt.hash(req.body.password, 10)
-            users.push({
-                id: Date.now().toString(), 
-                name: req.body.name,
-                email: req.body.email,
-                password: hashedPassword,
-            })
-            console.log(users); // Display newly registered in the console
-            res.redirect("/login")
-            
-        } catch (e) {
-            console.log(e);
-            res.redirect("/register")
-        }
+  var s = req.body.name;
+  var pass = req.body.password;
+  if (containsOnlyLetters(s) && passCheck(pass)) {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+      console.log(users);
+      res.redirect("/login");
+    } catch (e) {
+      console.log(e);
+      res.redirect("/register");
     }
-    else{
-        // return usernameError();
-        console.log("ONLY LETTERS IN USERNAME")
-    }
-})
+  } else {
+    console.log("ONLY LETTERS IN USERNAME");
+  }
+});
 
-function usernameError(){
-    alert("ONLY LETTERS IN USERNAME");
+function usernameError() {
+  alert("ONLY LETTERS IN USERNAME");
 }
-function emailError(){
-    alert("Invalid Email");
+function emailError() {
+  alert("Invalid Email");
 }
 
 function containsOnlyLetters(str) {
-    return /^[a-zA-Z]+$/.test(str);
+  return /^[a-zA-Z]+$/.test(str);
 }
 
-function passCheck(str){
-    if(str.length>8 || str.length<3)
-        return false;
-    return true;
+function passCheck(str) {
+  if (str.length > 8 || str.length < 3) return false;
+  return true;
 }
 
-// Routes
-app.get('/', checkAuthenticated, (req, res) => {
-    res.render("index.ejs",{name: req.user.name})
-})
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
+});
 
-app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render("Login-Signup-main/index.ejs")
-})
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("Login-Signup-main/index.ejs");
+});
 
-app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render("Login-Signup-main/index.ejs")
-})
-// End Routes
-
-// app.delete('/logout', (req, res) => {
-//     req.logOut()
-//     res.redirect('/login')
-//   })
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("Login-Signup-main/index.ejs");
+});
 
 app.delete("/logout", (req, res) => {
-    req.logout(req.user, err => {
-        if (err) return next(err)
-        res.redirect("/")
-    })
-})
+  req.logout(req.user, (err) => {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
 
-function checkAuthenticated(req, res, next){
-    if(req.isAuthenticated()){
-        return next()
-    }
-    res.redirect("/login")
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
 }
 
-function checkNotAuthenticated(req, res, next){
-    if(req.isAuthenticated()){
-        return res.redirect("/")
-    }
-    next()
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
 }
 
-app.listen(3000)
+app.listen(3000);
